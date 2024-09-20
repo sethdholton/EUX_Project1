f5bc9eac14e66d3bccbf0ac183eb5e41c5e118d1
Seth Holton <sethdholton@utexas.edu>
2024-09-19 19:23:32 -0500

    File upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+﻿
+
+var playButton;
+
+var drumNames = ["hho", "hh", "snare", "kick"];
+kit = new Tone.Players(
+  {"hho" : "/assets/samples/hho.mp3",
+   "hh" : "/assets/samples/hh.mp3",
+   "snare" : "/assets/samples/snare.mp3",
+   "kick" : "/assets/samples/kick.mp3"
+
+  });
+
+kit.toMaster();
+Tone.Transport.bpm.value = 180;
+Tone.Transport.scheduleRepeat(onBeat, "8n");
+
+function setup() {
+  createCanvas(600, 300);
+
+  playButton = createButton('Play');
+  playButton.position(550, 300);
+  playButton.mouseClicked(togglePlay);
+}
+
+function draw() {
+  kit.player("hho").start();
+}
+
+function togglePlay() {
+  kit.player("hho").start();
+}
+
+function keyPressed() {
+}
